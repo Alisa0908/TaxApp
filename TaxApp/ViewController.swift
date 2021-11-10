b5615af17f455d623e15e343d3a10251603d3b42
Alisa0908 <alisa0908piano@gmail.com>
2021-11-10 22:23:37 +0900

    動作確認済み

--- TaxApp/ViewController.swift
+++ TaxApp/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         super.viewDidLoad()
         itemTableView.delegate = self
         itemTableView.dataSource = self
-
         costsArray.removeAll()
+        
+        costField.addTarget(self, action: #selector(costFieldDidChange), for: .editingChanged)
+        costField.keyboardType = UIKeyboardType.numberPad
     }
     
     var cost:Double = 0
@@ -27,13 +29,32 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     @IBOutlet var taxController: UISegmentedControl!
     @IBOutlet var itemTableView: UITableView!
     
+    @objc func costFieldDidChange(sender: UITextField) {  //値の取得
+        
+        if costField.text != "" &&  Int(costField.text!) != nil {
+            if taxController.selectedSegmentIndex == 0 {
+                calc(tax:1.1)
+            } else {
+                calc(tax: 1.08)
+            }
+        } else {
+            costField.text = ""
+            showLabel.text = ""
+        }
+    }
+    
     @IBAction func taxChanger(_ sender: Any) {
         
-        if taxController.selectedSegmentIndex == 0 {
-            //↓で登録しているcalc関数を使っている
-            calc(tax: 1.1)
-        } else {
-            calc(tax: 1.08)
+        if costField.text != "" &&  Int(costField.text!) != nil {
+            if taxController.selectedSegmentIndex == 0 {
+                //↓で登録しているcalc関数を使っている
+                calc(tax: 1.1)
+            } else {
+                calc(tax: 1.08)
+            }        }
+        else {
+            costField.text = ""
+            showLabel.text = ""
         }
         
     }
@@ -41,6 +62,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     func calc(tax:Double) {
         
         //    costFieldに追加された数字をcostに代入
+        
         cost = Double(costField.text!)!
         //    税金の計算を行う
         addTaxCost = cost * Double(tax)
@@ -53,43 +75,45 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     
     @IBAction func addButton(_ sender: Any) {
-        costsArray.append(contentsOf: [addTaxCost])
-        print(costsArray)
-        UserDefaults.standard.set(costsArray, forKey: "item")
-
-        costField.text = ""
-        showLabel.text = ""
-
-        self.itemTableView.reloadData()
+        if addTaxCost != 0 {
+            costsArray.append(contentsOf: [addTaxCost])
+            print(costsArray)
+            UserDefaults.standard.set(costsArray, forKey: "item")
+            
+            costField.text = ""
+            showLabel.text = ""
+            
+            self.itemTableView.reloadData()
+        }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-            return 1
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return costsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = itemTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        
+        cell.textLabel?.text = "\(costsArray[indexPath.row])"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            costsArray.remove(at: indexPath.row)
+            
+            UserDefaults.standard.set(costsArray, forKey: "item")
+            
+            itemTableView.reloadData()
         }
-
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return costsArray.count
-        }
-
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = itemTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-
-            cell.textLabel?.text = "\(costsArray[indexPath.row])"
-
-            return cell
-        }
-
-        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-            if editingStyle == .delete {
-
-                    costsArray.remove(at: indexPath.row)
-
-                    UserDefaults.standard.set(costsArray, forKey: "item")
-
-                    itemTableView.reloadData()
-            }
-        }
+    }
     
     
 }
